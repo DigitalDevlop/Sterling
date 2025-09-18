6598c26c2b642912ffe92159904876229b7c0762
dilanjanranmalinda <126053448+dilanjanranmalinda@users.noreply.github.com>
2025-09-18 16:28:48 +0530

    add image

--- src/widget/click1.js
+++ src/widget/click1.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import image1 from './../images/1.png';
+import image1 from './../images/1.jpeg';
 
 const Click1 = () => {
   const [formData, setFormData] = useState({
